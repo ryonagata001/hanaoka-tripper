bd31aefc27c3886e815bd695235d5d43f86f1a66
ryo.nagata <ryo.nagata@chillnn.com>
2019-10-04 14:12:07 +0900

    add: WORKS TITLE

--- src/views/WORKS.vue
+++ src/views/WORKS.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="home">
-        <h1>HANAOKA TRIPPER(NEWS/MEDIA)</h1>
+        <h1>HANAOKA TRIPPER(WORKS)</h1>
         <downloader :names="namesB" title="B"></downloader>
         <downloader :names="namesC" title="C"></downloader>
         <downloader :names="namesD" title="D"></downloader>
@@ -8,15 +8,15 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import 'vue-croppa/dist/vue-croppa.css';
-import { readUploadedFileAsBase64 } from '@/util/readBase64';
-import Downloader from '@/components/Downloader.vue';
+import { Component, Vue } from "vue-property-decorator";
+import "vue-croppa/dist/vue-croppa.css";
+import { readUploadedFileAsBase64 } from "@/util/readBase64";
+import Downloader from "@/components/Downloader.vue";
 
 @Component({
     components: {
-        Downloader,
-    },
+        Downloader
+    }
 })
 export default class Home extends Vue {
     public namesB: Array<[number, number]> = [[960, 720]];
@@ -24,7 +24,7 @@ export default class Home extends Vue {
         [280, 460],
         [630, 630],
         [210, 470],
-        [630, 315],
+        [630, 315]
     ];
     public namesD: Array<[number, number]> = [[630, 830]];
 }
